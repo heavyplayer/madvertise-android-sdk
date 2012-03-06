e38436a9bdff38683fec59a5563cbf8df1111a59
Moritz Becker <moritz.becker@madvertise.de>
2012-03-06 14:59:10 +0100

    added not working example

--- madvertiseSDK/res/raw/pinger.js
+++ madvertiseSDK/res/raw/pinger.js
@@ -1,6 +1,12 @@
 // this does not work!!
 mraid_bridge.ping("blubbi");
 
+// nope, doesn't work!
+(function() {
+  mraid_bridge.ping("blubbiiiiii");
+}).call(this);
+
+
 // but from here, it's fine
 function pingCall() {
 	mraid_bridge.ping("blubbi");
